7d670f44d9cc53ad045ddcdf7a10bc40f082eb5f
gabrielemafrica <gabriele.ga@live.it>
2023-07-13 17:28:49 +0300

    creazione tabella e connessione

---
+++ database/migrations/2023_07_13_135420_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('azienda', 64);
+            $table -> string('stazione_di_partenza', 64);
+            $table -> string('stazione_di_arrivo', 64);
+            $table -> time('orario_di_partenza');
+            $table -> time('orario_di_arrivo');
+            $table -> string('codice_treno', 5) -> unique();
+            $table -> integer('numero_carrozze');
+            $table -> boolean('in_orario') -> default(true);
+            $table -> boolean('cancellato') -> default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Guest\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ MainController :: class, 'index']) -> name('hame');
