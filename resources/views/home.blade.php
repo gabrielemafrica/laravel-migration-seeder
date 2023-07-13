2d571ae24abf3786272dc1cef159613b30437e74
gabrielemafrica <gabriele.ga@live.it>
2023-07-13 21:49:33 +0300

    finale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,46 +8,49 @@
     <main class="py-4">
         <div class="container">
             <h1 class="text-center my-5">RIEPILOGO TRENI</h1>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">azienda</th>
-                        <th scope="col">stazione di partenza</th>
-                        <th scope="col">stazione di arrivo</th>
-                        <th scope="col">orario di partenza</th>
-                        <th scope="col">orario di arrivo</th>
-                        <th scope="col">codice treno</th>
-                        <th scope="col">numero di carrozze</th>
-                        <th scope="col">in orario</th>
-                        <th scope="col">cancellato</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($trains as $train)
+            <div class="table-container mb-5">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <th scope="row">{{ $train->azienda }}</th>
-                            <td>{{ $train->stazione_di_partenza }}</td>
-                            <td>{{ $train->stazione_di_arrivo }}</td>
-                            <td>{{ $train->orario_di_partenza }}</td>
-                            <td>{{ $train->orario_di_arrivo }}</td>
-                            <td>{{ $train->codice_treno }}</td>
-                            <td>{{ $train->numero_carrozze }}</td>
-                            @if ($train->in_orario)
-                                <td>in orario</td>
-                            @else
-                                <td>RITARDO</td>
-                            @endif
-                            @if ($train->cancellato)
-                                <td>SOPPRESSO</td>
-                            @else
-                                <td>REGOLARE</td>
-                            @endif
+                            <th scope="col">azienda</th>
+                            <th scope="col">stazione di partenza</th>
+                            <th scope="col">stazione di arrivo</th>
+                            <th scope="col">orario di partenza</th>
+                            <th scope="col">orario di arrivo</th>
+                            <th scope="col">codice treno</th>
+                            <th scope="col">numero di carrozze</th>
+                            <th scope="col">in orario</th>
+                            <th scope="col">cancellato</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody class="treni">
+                        @foreach ($trains as $train)
+                            <tr>
+                                <th scope="row">{{ $train->azienda }}</th>
+                                <td>{{ $train->stazione_di_partenza }}</td>
+                                <td>{{ $train->stazione_di_arrivo }}</td>
+                                <td>{{ substr($train->orario_di_partenza, 0, 5) }}</td>
+                                <td>{{ substr($train->orario_di_arrivo, 0, 5) }}</td>
+                                <td>{{ $train->codice_treno }}</td>
+                                <td>{{ $train->numero_carrozze }}</td>
+                                @if ($train->cancellato)
+                                    <td> - </td>
+                                @elseif ($train->in_orario)
+                                    <td>in orario</td>
+                                @else
+                                    <td>RITARDO</td>
+                                @endif
+                                @if ($train->cancellato)
+                                    <td>SOPPRESSO</td>
+                                @else
+                                    <td>REGOLARE</td>
+                                @endif
+                            </tr>
+                        @endforeach
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
 @endsection
